ead17660e29e40a0c7a13aa9571465a1c5bb7746
nodonoug@rand.org <nodonoug@rand.org>
2020-04-17 14:49:21 -0400

    Added 2-D DOA output option to triangulation measurement function.

--- +triang/measurement.m
+++ +triang/measurement.m
@@ -1,5 +1,5 @@
-function psi = measurement(x_sensor, x_source)
-% psi = measurement(x_sensor, x_source)
+function [psi, psi_elev] = measurement(x_sensor, x_source)
+% psi, psi_elev = measurement(x_sensor, x_source)
 %
 % Computes angle of arrival measurements.
 %
@@ -8,7 +8,8 @@ function psi = measurement(x_sensor, x_source)
 %   x_source    nDim x nSource array of source positions
 %
 % OUTPUTS:
-%   psi         nSensor x nSource array of AOA measurements
+%   psi         nSensor x nSource array of azimuth measurements (2D or 3D)
+%   psi_elev    nSensor x nSource array of elevation measurements (3D)
 %
 % Nicholas O'Donoughue
 % 1 July 2019
@@ -25,3 +26,11 @@ dx = reshape(x_source,nDim,1,nSource) - reshape(x_sensor,nDim,nSensor);
         % nDim x nSensor x nSource
         
 psi = reshape(atan2(dx(2,:,:),dx(1,:,:)),nSensor,nSource);
+
+if nDim1 == 3
+    % Compute elevation angle
+    psi_elev = reshape(atan2(dx(3,:,:),sqrt(dx(1,:,:).^2+dx(2,:,:).^2)),...
+                        nSensor,nSource);
+else
+    psi_elev = [];
+end
