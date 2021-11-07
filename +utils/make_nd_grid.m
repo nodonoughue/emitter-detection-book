47d42ace2a69adab4dcc71498d98fa6d0f05c614
Nicholas A. O'Donoughue <nodonoug@rand.org>
2021-11-07 06:51:09 -0500

    Add support for arbitrary parameter vector size to bestfix and ML solvers.

---
+++ +utils/make_nd_grid.m
@@ -0,0 +1,69 @@
+function [x_set, x_grid] = make_nd_grid(ctr,max_offset,spacing)
+% x_set = make_nd_grid(ctr,max_offset,spacing)
+%
+% Accepts a center value, maximum offset, and spacing vectors, of arbitrary
+% length n_dim, and generates a search grid over n_dim dimensions.
+%
+% The returned matrix x_set has n_dim columns and N rows, where N is the
+% product of the search vector in each dimension.
+%    n_elements = 1 + 2*max_offset./spacing.
+%    N = prod(n_elements)
+%
+% Inputs ctr, max_offset, and spacing must either be scalar, or have a
+% common number of elements.  They are assumed to be vectors (shape is not
+% preserved).
+%
+% Includes some error checking on the total array size allowable; which is
+% currently set at no more than 10% of MATLAB's maximum array size, to be
+% conservative.
+%
+% INPUTS:
+%   ctr         Scalar or n_dim vector of center point for each dimension
+%   max_offset  Scalar or n_dim vector of maximum search space offset from
+%               ctr for each dimension
+%   epsilon     Scalar or n_dim vector of search space resolution for each
+%               dimension
+%
+% OUTPUTS:
+%   x_full      n_dim x N matrix of coordinates that span the full search
+%               space
+%
+% Nicholas O'Donoughue
+% 7 Nov 2021
+
+%% Parse Inputs and Check for Max Array Size
+n_dims = numel(ctr);
+
+if numel(max_offset)==1
+    max_offset = max_offset*ones(n_dim,1);
+end
+
+if numel(spacing)==1
+    spacing = spacing * ones(n_dim,1);
+end
+
+assert(n_dims == numel(max_offset) && ...
+       n_dims == numel(spacing),...
+       'Search space dimensions do not match across specification of the center, search_size, and epsilon.');
+
+n_elements = 1+ 2*search_size./epsilon;
+
+% Check Search Size
+[user, ~] = memory;
+maxElements = user.MaxPossibleArrayBytes/10; % Set a conservative limit,
+                                             % since we'll need at least 2
+assert(prod(n_elements) < maxElements, 'Search size is too large; MATLAB is likely to crash or become unresponsive.');
+
+%% Initialize search space
+
+% dims is a cell array of dimensions, each of which contains a vector of
+% grid points along that dimension
+dims = arrayfun(@(x,x_mx,n) x + x_mx * linspace(-1,1,n),  ctr, max_offset, n_elements, 'UniformOutput',false);
+
+% Use ndgrid expansion; each element of dims_full is now a full n_dim 
+% dimensioned grid for one of the elements of x
+[x_grid{1:numel(dims)}] = ndgrid(dims{:}); %use comma-separated list expansion on both sides
+
+% Rearrange to an n_dim x N matrix
+x_grid_vec = cellfun(@(A) reshape(A,1,[]), x_grid, 'UniformOutput',false);
+x_set = cell2mat(x_grid_vec(:)).'; % n_dim x N
--- +utils/mlSoln.m
+++ +utils/mlSoln.m
@@ -1,4 +1,4 @@
-function [x_est,A,x_grid] = mlSoln(ell,x_ctr,search_size,epsilon)
+function [x_est,A, x_grid] = mlSoln(ell,x_ctr,search_size,epsilon)
 % function [x_est,A,x_grid] = mlSoln(ell,x_ctr,search_size,epsilon)
 %
 % Execute ML estimation through brute force computational methods.
@@ -20,34 +20,9 @@ function [x_est,A,x_grid] = mlSoln(ell,x_ctr,search_size,epsilon)
 % Nicholas O'Donoughue
 % 1 July 2019
 
-nDim = numel(x_ctr);
-if nDim <1 || nDim > 3
-    error('Number of spatial dimensions must be between 1 and 3');
-end
+[x_set, x_grid] = utils.make_nd_search_space(x_ctr, search_size, epsilon);
 
-if numel(search_size)==1
-    search_size = search_size*ones(nDim,1);
-end
-
-% Initialize search space
-xx = x_ctr(1) + (-search_size(1):epsilon:search_size(1));
-if nDim > 1
-    yy = x_ctr(2) + (-search_size(2):epsilon:search_size(2));
-    if nDim > 2
-        zz = x_ctr(3) + (-search_size(3):epsilon:search_size(3));
-        [XX,YY,ZZ] = ndgrid(xx,yy,zz);
-        x_set = [XX(:),YY(:),ZZ(:)]';
-        x_grid = {xx,yy,zz};
-    else
-        [XX,YY] = ndgrid(xx,yy);
-        x_set = [XX(:),YY(:)]';
-        x_grid = {xx,yy};
-    end
-else
-    x_set = xx(:)';
-    x_grid = xx;
-end
-
+% rearrange to a matrix, where each column is
 % Evaluate the likelihood function at each coordinate in the search space
 A = ell(x_set);
 
